6b5687970fa9ac06f5ef8f032cf70c4cfc8fa787
Tim Forkmann <tim.forkmann@msu-solutions.de>
2023-08-16 14:20:05 -0400

    fix view

--- src/Docs/View.fs
+++ src/Docs/View.fs
@@ -114,7 +114,6 @@ let private leftSide (p: Page) =
                           ]
                       ] ]
                 Daisy.menu [
-                    menu.compact
                     prop.className "flex flex-col p-4 pt-0"
                     prop.children [
                         Daisy.menuTitle [ Html.span "Docs" ]
@@ -136,13 +135,14 @@ let private inLayout state dispatch (title: string) (docLink: string) (p: Page) 
         theme.custom state.Theme
         prop.children [
             Daisy.drawer [
-                drawer.mobile
                 prop.children [
                     Daisy.drawerToggle [
                         prop.id "main-menu"
                     ]
-                    rightSide state dispatch title docLink elm
-                    leftSide p
+                    Daisy.drawerContent [
+                        rightSide state dispatch title docLink elm
+                        leftSide p
+                    ]
                 ]
             ]
         ]
